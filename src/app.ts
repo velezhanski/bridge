31edf3c9b3d1fbba900b0f1e38a6a3700adad4c5
Alex Filipenko <alexfilipenko719@gmail.com>
2020-10-19 10:37:51 +0300

    Made it support heroku

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,18 @@ import config from './config';
 async function startServer() {
   const app = express();
 
+  const port = process.env.PORT || 3000
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+
   const bot = new Telegraf(config.telegram.apiKey)
   bot.start((ctx) => ctx.reply('Добро пожаловать в сервис Bridge! Как вас зовут?'))
-
-  bot.help((ctx) => ctx.reply('Send me a sticker'))
-  bot.on('sticker', (ctx) => ctx.reply('👍'))
-  bot.on('message', (ctx) =>  ctx.reply(`Привет, ${ctx.message.text}!`)) 
+  bot.on('message', (ctx) =>  ctx.reply(`Привет, ${ctx.message.text}!`))
+  bot.on('sticker', (ctx) => ctx.reply('👍')) 
   bot.launch()
 
 }
