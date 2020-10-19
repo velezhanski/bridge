8aeba1b14a40cf9c7cad225b21b77340f9bd3adc
Alex Filipenko <alexfilipenko719@gmail.com>
2020-10-19 14:01:09 +0300

    removing possible error

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,7 @@ async function startServer() {
   app.use(bot.webhookCallback(`/bot${config.telegram.token}`));
   
   bot.start((ctx) => ctx.reply('Добро пожаловать в сервис Bridge! Как вас зовут?'))
-  bot.on('message', (ctx) =>  console.log("THIS IS THE TEXT:" + ctx.message.text))
-  // ctx.reply(`Привет, ${ctx.message.text}!`))
+  bot.on('message', (ctx) =>  ctx.reply(`Привет, друг!`))
   bot.on('sticker', (ctx) => ctx.reply('👍')) 
   bot.launch()
 
