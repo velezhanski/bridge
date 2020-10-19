84606205c70adb3a74dc7618abca31a0adc33a64
Alex Filipenko <alexfilipenko719@gmail.com>
2020-10-19 20:26:43 +0300

    Fixed the structure

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,12 @@
 import express from 'express';
-import {Telegraf} from 'telegraf';
-import config from './config';
+import { Container } from 'typedi';
+import TelegramBotService from './services/telegram'
 
 async function startServer() {
   const app = express();
-  const port = config.port || 3000
-  const bot = new Telegraf(config.telegram.token)
 
-
-  bot.telegram.setWebhook(`${config.heroku.url}/bot${config.telegram.token}`);
-  app.use(bot.webhookCallback(`/bot${config.telegram.token}`));
-  
-  bot.start((ctx) => ctx.reply('Добро пожаловать в сервис Bridge! Как вас зовут?'))
-  bot.on('message', (ctx) =>  ctx.reply(`Привет, ${ctx.message.text}!`))
-  bot.on('sticker', (ctx) => ctx.reply('👍')) 
-  bot.launch()
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-  });
+  const telegramBotServiceInstance = Container.get(TelegramBotService);
+  telegramBotServiceInstance.LaunchBotService(app);
 }
 
 startServer();
